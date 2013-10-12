89c8a2d209caf07fe56e6e31bf7a2c138d343c67
Twenty-Six medias, Inc. <julien@twenty-six-medias.com>
2013-10-11 23:38:26 -0400

    Structure change, better pagination

--- api/endpoints/user.js
+++ api/endpoints/user.js
@@ -1,70 +1,58 @@
 var _ 					= require('underscore');
+var qs 					= require("querystring");
 
 // Users
 function api() {
 	
 }
-api.prototype.init = function(Gamify){
+api.prototype.init = function(Gamify, callback){
 	var scope = this;
 	
 	this.Gamify = Gamify;
-	this.ready	= false;	// If the API ready? Avoid exceptions.
-	
-	this.mongo	= new this.Gamify.mongo({database:'fleetwit'});
-	this.mongo.init(function() {
-		scope.ready = true;
-	});
 	
 	// Return the methods
-	return {
-		find: function(params, callback) {
+	var methods = {
+		find: function(params, req, res, callback) {
 			
 			params	= _.extend({
 				perpage:	5,
 				page:		1
 			},params);
 			
-			if (scope.ready) {
-				scope.mongo.find(_.extend(params, {
-					collection:	"datastore",
-					query:		{}
-				}), callback);
+			scope.mongo.find(_.extend({
+				collection:	"datastore",
+				query:		{}
+			}, params), callback);
 				
-			} else {
-				callback(scope.Gamify.api.errorResponse('This API is not ready yet.'));
-			}
 		},
-		paginate: function(params, callback) {
+		paginate: function(params, req, res, callback) {
 			
 			params	= _.extend({
 				perpage:	5,
 				page:		1
 			},params);
 			
-			if (scope.ready) {
+			scope.mongo.paginate(_.extend({
+				collection:	"datastore",
+				query:		{}
+			}, params), function(response) {
+				var nextParam		= _.extend({},params);
+				nextParam.page 		= response.pagination.current+1;
+				var prevParam		= _.extend({},params);
+				prevParam.page		= response.pagination.current-1;
 				
-				scope.mongo.paginationInfo(_.extend(params, {
-					collection:	"datastore"
-				}), function(response) {
-					
-					response.current	= params.page;
-					
-					scope.mongo.find(_.extend(params, {
-						collection:	"datastore",
-						query:		{}
-					}), function(response2) {
-						callback({
-							pagination:	response,
-							data:		response2
-						});
-					});
-				});
-				
-				
-			} else {
-				callback(scope.Gamify.api.errorResponse('This API is not ready yet.'));
-			}
+				response.next		= response.pagination.current >= response.pagination.pages ? false : req.path+"?"+qs.stringify(nextParam);
+				response.previous	= response.pagination.current <= 1 ? false : req.path+"?"+qs.stringify(prevParam);
+				callback(response);
+			});
+			
 		}
 	};
+	
+	// Init a connection
+	this.mongo	= new this.Gamify.mongo({database:'fleetwit'});
+	this.mongo.init(function() {
+		callback(methods);
+	});
 }
 exports.api = api;
