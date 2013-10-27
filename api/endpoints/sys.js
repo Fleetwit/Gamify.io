01a681c8cd0bc36c2fa46fc0455030f1f86b9749
Twenty-Six medias, Inc. <julien@twenty-six-medias.com>
2013-10-27 00:38:48 -0400

    Import script, "sys" endpoint added.

---
+++ api/endpoints/sys.js
@@ -0,0 +1,110 @@
+var _ 					= require('underscore');
+var qs 					= require("querystring");
+
+// Users
+function api() {
+	
+}
+api.prototype.init = function(Gamify, callback){
+	var scope = this;
+	
+	this.Gamify = Gamify;
+	
+	// Return the methods
+	var methods = {
+		
+		
+		
+		
+		importRaces: {
+			require:		[],
+			auth:			'sys',
+			callback:		function(params, req, res, callback) {
+				
+				// Reset the data
+				scope.mongo.remove({
+					collection:	'clients'
+				}, function() {
+					console.log("\033[33mClients\033[0m removed.");
+					scope.mongo.remove({
+						collection:	'races'
+					}, function() {
+					console.log("\033[33mRaces\033[0m removed.");
+						
+						// Find the original race data
+						scope.mongo_old.find({
+							collection:	'clients',
+							query:		{}
+						}, function(clients) {
+							// Now we have the clients.
+							// Let's register them.
+							var i;
+							var stack	= new Gamify.stack();
+							console.log("clients",clients[0].data.clients.length);
+							console.log("\033[33m"+clients[0].data.clients.length+"\033[0m clients found.");
+							for (i=0;i<clients[0].data.clients.length;i++) {
+								stack.add(function(params, onProcessed) {
+									
+									// remove the races from the client data
+									delete params.client.races;
+									
+									scope.Gamify.api.execute("race","create_client", params.client, function(exec_response) {
+										var stackRace	= new Gamify.stack();
+										var j;
+										if (params.races && params.races.length) {
+											console.log("\033[33m"+params.races.length+"\033[0m races found for client \033[33m",params.client.name,"\033[0m");
+											for (j=0;j<params.races.length;j++) {
+												stackRace.add(function(params2, onProcessed) {
+													
+													// Copy the prizes to .prize
+													params2.race.prizes = params2.race.prices;
+													// Delete the prizes
+													delete params2.race.prices;
+													
+													scope.Gamify.api.execute("race","create", {
+														client:		params.client.uuid,
+														data:		params2.race
+													}, function(exec_response2) {
+														onProcessed();
+													});
+												}, {race: params.races[j]});
+											}
+											stackRace.process(function() {
+												console.log("\t> All races processed.");
+												onProcessed();
+											}, false);
+										} else {
+											console.log("No races found for client \033[33m",params.client.name,"\033[0m");
+											onProcessed();
+										}
+										
+									});
+								},{client:_.extend({},clients[0].data.clients[i]), races: clients[0].data.clients[i].races});
+							}
+							stack.process(function() {
+								callback({done: true});
+							}, false);
+						});
+						
+						
+					});
+				});
+				
+				
+
+				
+			}
+		}
+		
+	};
+	
+	// Init a connection
+	this.mongo		= new this.Gamify.mongo({database:Gamify.settings.db});
+	this.mongo_old	= new this.Gamify.mongo({database:'fleetwit'});
+	this.mongo.init(function() {
+		scope.mongo_old.init(function() {
+			callback(methods);
+		});
+	});
+}
+exports.api = api;
