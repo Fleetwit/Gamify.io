e711c5967a943b172d4c5ce72c9a43b5f983b43d
Twenty-Six medias, Inc. <julien@twenty-six-medias.com>
2013-11-17 19:51:38 -0200

    fix

--- api/endpoints/sys.js
+++ api/endpoints/sys.js
@@ -140,6 +140,7 @@ api.prototype.init = function(Gamify, callback){
 			status:			'dev',
 			version:		0.1,
 			callback:		function(params, req, res, callback) {
+				
 				scope.mongo_old.find({
 					collection:	'datastore',
 					query:		{
@@ -152,78 +153,90 @@ api.prototype.init = function(Gamify, callback){
 					var __stats = {};
 					
 					var importStack = new Gamify.stack();
+					
+					
 					_.each(oldusers, function(olduser) {
 						
-						console.log("User:",olduser);
-						if (olduser.privatedata) {
-							olduser.email = olduser.privatedata.email;
-							importStack.add(function(params, onProcessed) {
-								// Get the user's uid
-								if (params.olduser.email) {
-									scope.mongo.find({
-										collection:	"users",
-										query:	{
-											email:	params.olduser.email
-										}
-									}, function(users) {
-										
-										console.log("users found:",users.length);
-										
-										if (users.length > 0) {
+						importStack.add(function(params, onProcessed) {
+							
+							// Get the user's SQL data
+							Gamify.sql.query("select * from users where id="+params.olduser.uid, function(err, rows, fields) {
+								if (!err && rows.length > 0 && rows[0].id > 0) {
+									// extend the user's local data
+									params.olduser.sql 		= rows[0];
+									params.olduser.email 	= rows[0].email;
+							
+							
+									// Get the user's uid
+									if (params.olduser.email) {
+										scope.mongo.find({
+											collection:	"users",
+											query:	{
+												email:	params.olduser.email
+											}
+										}, function(users) {
 											
-											// save the new uid as "uuid"
-											params.olduser.uuid = users[0].uid;
+											console.log("users found:",users.length);
 											
-											
-											// Add each user import op to a new layer in the stack
-										
-											
-											if (params.olduser.racedata && params.olduser.racedata.length > 0) {
+											if (users.length > 0) {
 												
-												var groupedData = _.groupBy(params.olduser.racedata, function(item) {
-													return item["race"];
-												});
+												// save the new uid as "uuid"
+												params.olduser.uuid = users[0].uid;
 												
 												
-												_.each(params.olduser.racedata, function(racereg) {
-													if (racereg.type && racereg.type == "registration") {
-														// Register for the race
-														console.log(params.olduser.email+" -> "+Gamify.data.oldraces.getByUuid(racereg.race).alias);
-														
-														scope.Gamify.api.execute("user","log", {
-															authtoken:		Gamify.settings.systoken,
-															__auth:			params.olduser.uuid,
-															__authcheck:	Gamify.settings.systoken,
-															data:			{
-																action:	"race.register",
-																race:	Gamify.data.oldraces.getByUuid(racereg.race).alias
-															}
-														}, function(response) {
-															if (!__stats[params.olduser.email]) {
-																__stats[params.olduser.email] = [];
-															}
-															__stats[params.olduser.email].push(Gamify.data.oldraces.getByUuid(racereg.race).alias);
-															
-														});
-													}
+												// Add each user import op to a new layer in the stack
+											
+												
+												if (params.olduser.racedata && params.olduser.racedata.length > 0) {
 													
-												});
-												
-												onProcessed();
+													var groupedData = _.groupBy(params.olduser.racedata, function(item) {
+														return item["race"];
+													});
+													
+													
+													_.each(params.olduser.racedata, function(racereg) {
+														if (racereg.type && racereg.type == "registration") {
+															// Register for the race
+															console.log(params.olduser.email+" -> "+Gamify.data.oldraces.getByUuid(racereg.race).alias);
+															
+															scope.Gamify.api.execute("user","log", {
+																authtoken:		Gamify.settings.systoken,
+																__auth:			params.olduser.uuid,
+																__authcheck:	Gamify.settings.systoken,
+																data:			{
+																	action:	"race.register",
+																	race:	Gamify.data.oldraces.getByUuid(racereg.race).alias
+																}
+															}, function(response) {
+																if (!__stats[params.olduser.email]) {
+																	__stats[params.olduser.email] = [];
+																}
+																__stats[params.olduser.email].push(Gamify.data.oldraces.getByUuid(racereg.race).alias);
+																
+															});
+														}
+														
+													});
+													
+													onProcessed();
+												} else {
+													onProcessed();
+												}
+															
+													
+												//
 											} else {
 												onProcessed();
 											}
-														
-												
-											//
-										} else {
-											onProcessed();
-										}
-									});
+										});
+									} else {
+										onProcessed();
+									}
 								} else {
 									onProcessed();
 								}
-							},{olduser: olduser});
+							});
+						},{olduser: olduser});
 						}
 					});
 					
